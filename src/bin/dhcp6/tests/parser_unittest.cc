b76cbf31df879d83a8b09d8a69be84c8aab1d2d2
Francis Dupont <fdupont@isc.org>
2020-11-18 11:55:06 +0100

    [#1405] Updated ParserTest.mapEntries

--- src/bin/dhcp6/tests/parser_unittest.cc
+++ src/bin/dhcp6/tests/parser_unittest.cc
@@ -747,6 +747,7 @@ TEST(ParserTest, mapEntries) {
     ElementPtr sample_json = Element::createList();
     loadFile(sample_dir + "advanced.json", sample_json);
     loadFile(sample_dir + "all-keys.json", sample_json);
+    loadFile(sample_dir + "all-keys-netconf.json", sample_json);
     loadFile(sample_dir + "duid.json", sample_json);
     loadFile(sample_dir + "reservations.json", sample_json);
     KeywordSet sample_keys = {
